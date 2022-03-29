59130d86bb7a7d2d3041439dd8d00ccb86bf517e
Jonathan da Silva Chagas <sich.nathan@gmail.com>
2022-03-29 10:27:04 -0300

    Readme.py
    Olá, eu sou Jonathan e essa é minha apresentação em Python, meu foco hoje é aprender Python, Banco de Dados e Ciencia de Dados.

---
+++ Readme.py
@@ -0,0 +1,19 @@
+n1=input ( 'Quem é você?')
+n2=input ('Qual seu objetivo?')
+n3=input ('O que está aprendendo?')
+n4=input ('Qual a sua colaboração?')
+n5=input ('Qual seu contato?')
+print('Eu sou', n1)
+print('Estou interessado em', n2)
+print('Atualmente estou aprendendo', n3)
+print('Colaboro', n4)
+print('Meu email é', n5)
+
+
+
+Quem é você?Jonathan da Silva Chagas
+Qual seu objetivo?Aprender programação e trabalhar na área de dados
+O que está aprendendo?Python
+Qual a sua colaboração?Atualmente estou iniciando, porém com o tempo pretendo colaborar com meu projetos
+Qual seu contato?sich.nathan@gmail.com
+Eu sou Jonathan da Silva Chagas
